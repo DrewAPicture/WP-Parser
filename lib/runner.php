72c1fd366c4cf8e63f7597194ea6e68f2ac9adab
DrewAPicture <drew@10up.com>
2014-10-13 02:08:20 -0600

    Introduce preliminary functionality for parsing hash notations inside @param tags.
    Introduces parse_hashes() for converting a string-based hash notation into a parsed array of data. Leverages the phpDocumentor\Reflection\DocBlock\Tag\ParamTag class to parse `@type` tags.

--- lib/runner.php
+++ lib/runner.php
@@ -2,6 +2,11 @@
 
 namespace WP_Parser;
 
+use phpDocumentor\Reflection\DocBlock\Tag;
+use phpDocumentor\Reflection\DocBlock\Tag\ParamTag;
+
+Tag::registerTagHandler( 'type', 'phpDocumentor\Reflection\DocBlock\Tag\ParamTag' );
+
 function get_wp_files( $directory ) {
 	$iterableFiles = new \RecursiveIteratorIterator(
 		new \RecursiveDirectoryIterator( $directory )
@@ -126,19 +131,29 @@ function export_docblock( $element ) {
 	);
 
 	foreach ( $docblock->getTags() as $tag ) {
+		$content = preg_replace( '/[\n\r]+/', ' ', $tag->getDescription() );
+
+		if ( 'param' == $tag->getName() && '{' == $content[0] ) {
+			$content = parse_hashes( $content, $tag, $docblock );
+		}
+
 		$t = array(
 			'name'    => $tag->getName(),
-			'content' => preg_replace( '/[\n\r]+/', ' ', $tag->getDescription() ),
+			'content' => $content,
 		);
+
 		if ( method_exists( $tag, 'getTypes' ) ) {
 			$t['types'] = $tag->getTypes();
 		}
+
 		if ( method_exists( $tag, 'getVariableName' ) ) {
 			$t['variable'] = $tag->getVariableName();
 		}
+
 		if ( method_exists( $tag, 'getReference' ) ) {
 			$t['refers'] = $tag->getReference();
 		}
+
 		if ( 'since' == $tag->getName() && method_exists( $tag, 'getVersion' ) ) {
 			$version = $tag->getVersion();
 			if ( !empty( $version ) ) {
@@ -272,4 +287,112 @@ function export_uses( array $uses ) {
 	}
 
 	return $out;
+}
+
+/**
+ * Parse the given hash notation string into an associative array.
+ *
+ * @todo Make this not suck so bad.
+ *
+ * @param string   $content  Parameter tag content.
+ * @param Tag      $tag      Tag object.
+ * @param DocBlock $docblock DocBlock object.
+ *
+ * @return array Hash notation information as an associative array.
+ */
+function parse_hashes( $content, $tag, $docblock ) {
+	$text = trim( substr( $content, 1, -1 ) );
+
+	// Temporarily make the closing braces parsable.
+	$text = str_replace( '    }', '@type }', $text );
+
+	// New lines (not really necessary, makes debugging easier).
+	$text = str_replace( '@type', "\n@type", $text );
+	$parts = explode( "\n", $text );
+
+	$hash = array();
+
+	$index = 0;
+	$first = $second = $third = false;
+
+	foreach ( $parts as $part ) {
+		$has_open_brace  = strpos( $part, '{' ) && ! strpos( $part, '{@' );
+		$has_close_brace = strpos( $part, ' }' );
+		$has_var         = preg_match( '/\$\w+/', $part, $var_matches );
+
+		$part = trim( preg_replace( '/\s+/', ' ', $part ) );
+
+		$has_var_and_match = ( $has_var && ! empty( $var_matches[0] ) );
+
+		// Descriptions.
+		if ( ! $has_open_brace && ! $has_var && ! $first ) {
+			// Top-level description.
+			$hash['content'] = $part;
+			continue;
+		}
+
+		if ( $has_close_brace ) {
+			if ( $first && ! $second ) {
+				$first = false;
+			} elseif ( $first && $second && ! $third ) {
+				$second = false;
+			} elseif ( $first && $second && $third ) {
+				$third = false;
+			}
+			continue;
+		}
+
+		// Setup levels.
+		if ( $has_open_brace ) {
+			if ( $has_var_and_match ) {
+				$type = ParamTag::createInstance( trim( $part ), $docblock );
+			} else {
+				$type = false;
+			}
+
+			$start_level_meta = array(
+				'content' => $type ? $type->getDescription() : '',
+				'types'   => $type ? $type->getTypes() : array( 'array' ),
+			);
+
+			if ( ! $first ) {
+				$first = true;
+				$first_level_key = $has_var_and_match ? $var_matches[0] : $index;
+
+				$hash[ $first_level_key ] = $start_level_meta;
+			} elseif ( $first && ! $second ) {
+				$second = true;
+				$second_level_key = $has_var_and_match ? $var_matches[0] : $index;
+
+				$hash[ $first_level_key ][ $second_level_key ] = $start_level_meta;
+			} elseif ( $second && ! $third ) {
+				$third = true;
+				$third_level_key = $has_var_and_match ? $var_matches[0] : $index;
+
+				$hash[ $first_level_key ][ $second_level_key ][ $third_level_key ] = $start_level_meta;
+			}
+			$index++;
+			continue;
+		}
+
+		$type = ParamTag::createInstance( trim( $part ), $docblock );
+
+		$var_key = $type->getVariableName();
+
+		$mid_level_meta = array(
+			'content' => $type->getDescription(),
+			'types'   => $type->getTypes(),
+		);
+
+		if ( ! $first ) {
+			$hash[ $var_key ] = $mid_level_meta;
+		} elseif ( $first && ! $second ) {
+			$hash[ $first_level_key ][ $var_key ] = $mid_level_meta;
+		} elseif ( $first && $second && ! $third ) {
+			$hash[ $first_level_key ][ $second_level_key ][ $var_key ] = $mid_level_meta;
+		} elseif ( $first && $second && $third ) {
+			$hash[ $first_level_key ][ $second_level_key ][ $third_level_key ][ $var_key ] = $mid_level_meta;
+		}
+	}
+	return $hash;
 }
